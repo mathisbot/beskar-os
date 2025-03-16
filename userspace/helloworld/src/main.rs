b4726bf8d1c1a0c5d42c32ad89f063f5d5a317ea
Mathis Bottinelli <mathis.bottinelli@student-cs.fr>
2025-03-16 21:14:12 +0100

    beskar-lib: added rand() function

--- userspace/helloworld/src/main.rs
+++ userspace/helloworld/src/main.rs
@@ -1,13 +1,13 @@
 #![no_std]
 #![no_main]
 
-use beskar_lib::io::print;
+use beskar_lib::{io::print, rand::rand};
 
 beskar_lib::entry_point!(main);
 
 fn main() {
     print("Hello, userspace!");
 
-    let mut buffer = [42_u8; 16];
-    beskar_lib::rand::rand(&mut buffer);
+    // Safety: any 8 random bytes are valid u64 values.
+    let _random_u64 = unsafe { rand::<u64>() };
 }
