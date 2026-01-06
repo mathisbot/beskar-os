3b1729622cfecd841f4edf85099353b7ba83bbf5
Mathis Bottinelli <mathis.bottinelli@student-cs.fr>
2026-01-06 19:53:21 +0100

    refactor(beskar-core): improve binary size
    remove non basic/latin unicode charater rasters from binaries, effectively reducing binary size of ~200 KiB

--- beskar-core/src/video/writer/chars.rs
+++ beskar-core/src/video/writer/chars.rs
@@ -7,7 +7,7 @@ const CHAR_HEIGHT_INTERNAL: RasterHeight = RasterHeight::Size20;
 pub const CHAR_HEIGHT: u16 = CHAR_HEIGHT_INTERNAL.val() as _;
 #[expect(clippy::cast_possible_truncation, reason = "No truncation here")]
 pub const CHAR_WIDTH: u16 = get_raster_width(FontWeight::Regular, CHAR_HEIGHT_INTERNAL) as _;
-const BACKUP_CHAR: char = '�';
+const BACKUP_CHAR: char = '?';
 
 #[must_use]
 #[inline]
